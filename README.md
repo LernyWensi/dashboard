826fe2a1a0c981c5a1513b9d00298c557494856f
Lerny <thelerny@gmail.com>
2024-09-29 19:49:40 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Dashboard
+
+This project is a part of [The Odin Project's Intermediate HTML and CSS course](https://www.theodinproject.com/paths/full-stack-javascript/courses/intermediate-html-and-css).
+
+> [Live Demo](https://lernywensi.github.io/dashboard)
+
+## Built with
+
+-   HTML
+-   CSS
+-   JavaScript
+
+## Task
+
+-   The objective was to create a dashboard based on the provided reference, utilizing both `grid` and `flexbox`, with a primary emphasis on `grid`.
+-   Responsiveness was an optional task, which has been successfully completed.
+
+## Preview | [Live](https://lernywensi.github.io/dashboard)
+
+![Desktop Full](preview.png)
+
+## Provided reference
+
+![Desktop Full](reference.png)
---
+++ script.js
@@ -0,0 +1,250 @@
+const PROJECTS = Object.freeze([
+    {
+        title: 'Super Cool Project',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elementum pharetra sem vitae ullamcorper.',
+    },
+    {
+        title: 'Stellar Innovations',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    {
+        title: 'Eco-Friendly Solutions',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    },
+    {
+        title: 'HealthTech Revolution',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    },
+    {
+        title: 'Creative Arts Hub',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+    {
+        title: 'Smart City Initiative',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pretium tincidunt lacus, ut aliquam massa nisl quis neque.',
+    },
+    {
+        title: 'Culinary Adventures',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus luctus egestas leo, sit amet ultricies sapien.',
+    },
+    {
+        title: 'Digital Learning Platform',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in libero ut nisl tincidunt scelerisque.',
+    },
+    {
+        title: 'Fitness for All',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.',
+    },
+    {
+        title: 'Wildlife Conservation Project',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque volutpat condimentum velit, id venenatis a condimentum vitae.',
+    },
+    {
+        title: 'Future of Work',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+    },
+]);
+
+const ANNOUNCEMENTS = Object.freeze([
+    {
+        title: 'Site Maintenance',
+        description:
+            'Vivamus massa felis, egestas vitae leo non, tempus auctor metus. Aenean at ultricies turpis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam sed molestie ante. Vivamus vitae sem blandit, lacinia tellus sed, egestas ipsum. Vestibulum vitae euismod arcu.',
+    },
+    {
+        title: 'New Feature Launch',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    },
+    {
+        title: 'Security Update',
+        description:
+            'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+    {
+        title: 'Holiday Hours',
+        description:
+            'Curabitur pretium tincidunt lacus. Nulla gravida orci a odio, et feugiat tellus malesuada sed. Donec libero. Suspendisse bibendum, nunc et interdum sagittis, nisi erat porttitor ligula, eget lacinia odio sem nec elit.',
+    },
+    {
+        title: 'User Feedback Survey',
+        description:
+            'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
+    },
+    {
+        title: 'System Upgrade',
+        description:
+            'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.',
+    },
+    {
+        title: 'New Blog Post',
+        description:
+            'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.',
+    },
+    {
+        title: 'Community Event',
+        description:
+            'Similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus.',
+    },
+    {
+        title: 'Maintenance Reminder',
+        description:
+            'Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus.',
+    },
+    {
+        title: 'Referral Program',
+        description:
+            'On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue.',
+    },
+    {
+        title: 'App Update Available',
+        description:
+            'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus.',
+    },
+]);
+
+const TRENDS = Object.freeze([
+    {
+        avatar: 'assets/circle.svg',
+        usertag: 'tegan',
+        project: 'World Peace Builder',
+    },
+    {
+        avatar: 'assets/triangle.svg',
+        usertag: 'james',
+        project: 'Eco-Friendly Initiative',
+    },
+    {
+        avatar: 'assets/star.svg',
+        usertag: 'sophia',
+        project: 'AI for Good',
+    },
+    {
+        avatar: 'assets/square.svg',
+        usertag: 'liam',
+        project: 'Community Garden Project',
+    },
+    {
+        avatar: 'assets/circle.svg',
+        usertag: 'ava',
+        project: 'Clean Water Access',
+    },
+    {
+        avatar: 'assets/triangle.svg',
+        usertag: 'noah',
+        project: 'Mental Health Awareness',
+    },
+    {
+        avatar: 'assets/star.svg',
+        usertag: 'olivia',
+        project: 'Wildlife Conservation',
+    },
+    {
+        avatar: 'assets/square.svg',
+        usertag: 'elijah',
+        project: 'Renewable Energy Solutions',
+    },
+    {
+        avatar: 'assets/circle.svg',
+        usertag: 'mia',
+        project: 'Food Security Program',
+    },
+    {
+        avatar: 'assets/triangle.svg',
+        usertag: 'isabella',
+        project: 'Digital Literacy Campaign',
+    },
+    {
+        avatar: 'assets/star.svg',
+        usertag: 'jacob',
+        project: 'Disaster Relief Fund',
+    },
+    {
+        avatar: 'assets/square.svg',
+        usertag: 'charlotte',
+        project: 'Youth Empowerment Initiative',
+    },
+]);
+
+const projects = document.querySelector('#projects');
+const announcements = document.querySelector('#announcements');
+const trends = document.querySelector('#trending');
+
+function getProjectHtml(title, description) {
+    return `
+        <a href="#">
+            <h3>${title}</h3>
+            <p>${description}</p>
+            <menu>
+                <li>
+                    <button type="button">
+                        <span class="material-symbols-rounded">star</span>
+                    </button>
+                </li>
+                <li>
+                    <button type="button">
+                        <span class="material-symbols-rounded">visibility</span>
+                    </button>
+                </li>
+                <li>
+                    <button type="button">
+                        <span class="material-symbols-rounded">share</span>
+                    </button>
+                </li>
+            </menu>
+        </a>
+    `;
+}
+
+function getAnnouncementHtml(title, description) {
+    return `
+        <a href="#">
+            <h3>${title}</h3>
+            <p>${description}</p>
+        </a>
+    `;
+}
+
+function getTrendHtml(avatar, usertag, project) {
+    return `
+        <a class="profile" href="#">
+            <img class="avatar" src="${avatar}" alt="Avatar" />
+            <section>
+                <p class="usertag">@${usertag}</p>
+                <p class="project">${project}</p>
+            </section>
+        </a>
+    `;
+}
+
+function createItemFromHtml(html) {
+    const item = document.createElement('li');
+    item.innerHTML = html;
+    return item;
+}
+
+PROJECTS.forEach(({ title, description }) => {
+    const project = createItemFromHtml(getProjectHtml(title, description));
+    projects.querySelector('ul').appendChild(project);
+});
+
+ANNOUNCEMENTS.forEach(({ title, description }) => {
+    const announcement = createItemFromHtml(getAnnouncementHtml(title, description));
+    announcements.querySelector('ul').appendChild(announcement);
+});
+
+TRENDS.forEach(({ avatar, usertag, project }) => {
+    const trend = createItemFromHtml(getTrendHtml(avatar, usertag, project));
+    trends.querySelector('ul').appendChild(trend);
+});
